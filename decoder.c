a832243323211bdeebac793f8ca317d9fd4c6d8f
administrator-11111 <cuentasamp2022@gmail.com>
2025-07-07 20:56:43 -0400

    decoder finished

--- decoder.c
+++ decoder.c
@@ -1,16 +1,11 @@
+/* Autores: Iván Maldonado, Yeral Méndez. */
 #include <stdio.h>
 #include <string.h>
 
-void inicializar_arreglo(char *, int);
 void lee_original(char *, int *);
 void inicializa_alfabeto(char *);
-int posicion_caracter(int, int, int);
-int verificar_multiplo(char, int);
 void decodificar(char *, char *, char *, int);
 void graba_mensaje(char *);
-void primera_etapa(char *, char *, int);
-void segunda_etapa(char *, char *, int);
-
 
 int main(){
     char original[100];
@@ -18,18 +13,16 @@ int main(){
     char codificado[100];
     int N;
     lee_original(original, &N);
-    printf("%d#%s\n", N, original);
     inicializa_alfabeto(alfabeto);
     decodificar(original, codificado, alfabeto, N);
     graba_mensaje(codificado);
     return 0;
 }
 
-void inicializar_arreglo(char *arreglo, int tam) {
-    int i;
-    for (i = 0; i < tam; i++) {
-        arreglo[i] = '\0';
-    }
+int posicion_caracter(int posicion, int clave, int tam) {
+    int nueva_posicion;
+    nueva_posicion = (posicion + (clave % tam)) % tam;
+    return nueva_posicion;
 }
 
 void lee_original(char *original, int *clave) {
@@ -69,30 +62,18 @@ void inicializa_alfabeto(char *alfabeto) {
     }
 }
 
-int posicion_caracter(int posicion, int clave, int tam) {
-    int nueva_posicion;
-    nueva_posicion = posicion + clave;
-    if (nueva_posicion >= tam) {
-        nueva_posicion -= tam;
-    }
-    return nueva_posicion;
-}
-
 void primera_etapa(char *original, char *codificado, int clave) {
     char alfabeto[48];
     int tamano_alfabeto, i, j;
     tamano_alfabeto = 47;
-    i = 0;
     inicializa_alfabeto(alfabeto);
-    inicializar_arreglo(codificado, 100);
+    i = 0;
     while (original[i] != '\0') {
         for(j = 0; j < tamano_alfabeto; j++) {
             if ((original[i] == alfabeto[j])) {
-                if (verificar_multiplo(original[i], clave) == 1) {
-                    int pos = posicion_caracter(j, clave, tamano_alfabeto);
-                    if (j % 2 == 0 || j == 0) {
-                        codificado[i] = alfabeto[pos];
-                    }
+                int pos = posicion_caracter(j, clave, tamano_alfabeto);
+                if (j % 2 == 0 || j == 0) {
+                    codificado[i] = alfabeto[pos];
                 } else {
                     codificado[i] = original[i];
                 }
@@ -105,10 +86,13 @@ void primera_etapa(char *original, char *codificado, int clave) {
 void segunda_etapa(char *precodificado, char *decodificado, int clave) {
     char alfabeto[48], resultado[100], aux[100];
     int tamano_alfabeto, i, j;
-    inicializa_alfabeto(alfabeto);
-    inicializar_arreglo(resultado, 100);
     tamano_alfabeto = 47;
+    for (i = 0; i < 100; i++) {
+        resultado[i] = '\0';
+    }
     i = 0;
+    inicializa_alfabeto(alfabeto);
+
     while (precodificado[i] != '\0') {
         for (j = 0; j < tamano_alfabeto; j++) {
             if (precodificado[i] == alfabeto[j]) {
@@ -124,9 +108,8 @@ void segunda_etapa(char *precodificado, char *decodificado, int clave) {
 
 void decodificar(char *original, char *codificado, char *alfabeto, int clave) {
     primera_etapa(original, codificado, clave);
-    printf("1. original: %s, codificado: %s\n", original, codificado);
+    printf("\nEtapa 1: %s\n", codificado);
     segunda_etapa(codificado, codificado, clave);
-    printf("2. original: %s, codificado: %s\n", original, codificado);
 }
 
 void graba_mensaje(char *mensaje) {
@@ -136,23 +119,4 @@ void graba_mensaje(char *mensaje) {
         fprintf(archivo, "%s", mensaje);
         fclose(archivo);
     }
-}
-
-int verificar_multiplo(char mensaje, int clave) {
-    char alfabeto[48];
-    int tamano_alfabeto = 47, retorno, i, j;
-    inicializa_alfabeto(alfabeto);
-    retorno = 0;
-    i = 0;
-    for (j = 0; j < tamano_alfabeto; j++) {
-        if (mensaje == alfabeto[j] && (j % 2 != 0)) {
-            int pos = posicion_caracter(j, clave, tamano_alfabeto);
-            //mensaje = alfabeto[pos];
-            if (pos % 2 == 0 || pos == 0) {
-                retorno = 1;
-            }
-            printf("%c[%d] -> %c[%d] = %d\n", alfabeto[j], j, alfabeto[pos], pos, retorno);
-        }
-    }
-    return retorno;
 }
